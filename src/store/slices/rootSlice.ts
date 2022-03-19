a7b5d942b02e687153288ee5cfe8634922d6d340
Norbert Pascu <58431719+NPascu6@users.noreply.github.com>
2022-03-19 01:11:08 +0100

    first commit

---
+++ src/store/slices/rootSlice.ts
@@ -0,0 +1,19 @@
+import {combineReducers} from "@reduxjs/toolkit";
+import {persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import appReducer from "./app/appSlice";
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+
+const appPersistConfig: any = {
+    key: 'app',
+    storage: storage,
+    stateReconciler: autoMergeLevel2
+};
+
+const rootSlice = combineReducers({
+    app: persistReducer(appPersistConfig, appReducer),
+});
+
+export type RootState = ReturnType<typeof rootSlice>;
+
+export default rootSlice;
